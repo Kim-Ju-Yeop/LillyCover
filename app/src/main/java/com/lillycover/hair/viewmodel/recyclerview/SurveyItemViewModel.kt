006007970b301e627062b994ab186f257f1ce6dc
Kim-Ju-Yeop <kjy031104@gmail.com>
2021-01-15 11:03:30 +0900

    [ADD] RecyclerView 핵심 플레이트 코드 작성

---
+++ app/src/main/java/com/lillycover/hair/viewmodel/recyclerview/SurveyItemViewModel.kt
@@ -0,0 +1,5 @@
+package com.lillycover.hair.viewmodel.recyclerview
+
+import com.lillycover.hair.base.viewmodel.BaseViewModel
+
+class SurveyItemViewModel : BaseViewModel()
---
+++ app/src/main/java/com/lillycover/hair/widget/recyclerview/RecyclerViewViewHolder.kt
@@ -0,0 +1,6 @@
+package com.lillycover.hair.widget.recyclerview
+
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerViewViewHolder(val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root)
