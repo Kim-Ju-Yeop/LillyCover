f0e81acc26211794e00655d6cd2f49588f763aef
Kim-Ju-Yeop <kjy031104@gmail.com>
2021-01-20 15:45:50 +0900

    [ADD] SurveyItem RecyclerView 구현

--- app/src/main/java/com/lillycover/hair/viewmodel/activity/SurveyViewModel.kt
+++ app/src/main/java/com/lillycover/hair/viewmodel/activity/SurveyViewModel.kt
@@ -1,18 +1,29 @@
 package com.lillycover.hair.viewmodel.activity
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MutableLiveData
+import com.lillycover.hair.R
 import com.lillycover.hair.base.viewmodel.BaseViewModel
+import com.lillycover.hair.network.model.response.SurveyQuestionResponse
 import com.lillycover.hair.network.repositoryImpl.SurveyRepositoryImpl
+import com.lillycover.hair.viewmodel.recyclerview.SurveyItemViewModel
+import com.lillycover.hair.widget.recyclerview.RecyclerViewItem
 
 class SurveyViewModel @ViewModelInject constructor(
     val surveyRepositoryImpl: SurveyRepositoryImpl
 ) : BaseViewModel() {
 
-    init {
-        getQuestion()
-    }
+    val surveyItemList = MutableLiveData<ArrayList<RecyclerViewItem>>()
 
-    fun getQuestion() {
+    init {
         surveyRepositoryImpl.getQuestion()
     }
+
+    fun setSurveyItemList(surveyQuestionResponse: List<SurveyQuestionResponse>) {
+        surveyItemList.value = surveyQuestionResponse.toRecyclerViewItemList() as ArrayList<RecyclerViewItem>
+    }
+
+    private fun List<SurveyQuestionResponse>.toRecyclerViewItemList() = map { it.toViewModel() }
+    private fun SurveyQuestionResponse.toViewModel() = SurveyItemViewModel(this).toRecyclerViewItem()
+    private fun SurveyItemViewModel.toRecyclerViewItem() = RecyclerViewItem(this, null, R.layout.item_survey)
 }
