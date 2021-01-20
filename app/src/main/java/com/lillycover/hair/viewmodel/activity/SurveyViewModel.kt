c0fd02f03b0a1d47e03a71794d117ac8cdf23851
Kim-Ju-Yeop <kjy031104@gmail.com>
2021-01-20 14:32:28 +0900

    [ADD] SurveyQuestion 데이터 조회

--- app/src/main/java/com/lillycover/hair/viewmodel/activity/SurveyViewModel.kt
+++ app/src/main/java/com/lillycover/hair/viewmodel/activity/SurveyViewModel.kt
@@ -1,5 +1,18 @@
 package com.lillycover.hair.viewmodel.activity
 
+import androidx.hilt.lifecycle.ViewModelInject
 import com.lillycover.hair.base.viewmodel.BaseViewModel
+import com.lillycover.hair.network.repositoryImpl.SurveyRepositoryImpl
 
-class SurveyViewModel : BaseViewModel()
+class SurveyViewModel @ViewModelInject constructor(
+    val surveyRepositoryImpl: SurveyRepositoryImpl
+) : BaseViewModel() {
+
+    init {
+        getQuestion()
+    }
+
+    fun getQuestion() {
+        surveyRepositoryImpl.getQuestion()
+    }
+}
